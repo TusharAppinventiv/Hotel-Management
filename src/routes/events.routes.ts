d4fc7785d3aea08316039a609b34b16a30350f32
TusharAppinventiv <tushar.vatsa@appinventiv.com>
2023-08-29 10:53:39 +0530

    commited

--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -3,10 +3,11 @@ import { EventCreatingController } from "../controllers/events.controller";
 import express from 'express'
 import { verifyAdmin } from "../middlewares/admin.auth";
 import { verifyUser } from "../middlewares/users.auth";
+import { verifyAdminOrUser } from "../middlewares/verifyAdminOrUser";
 
 const router = express.Router();
 router.post('/create',verifyAdmin, eventValidator, EventCreatingController.createEvent);
-router.get('/getEvents',verifyAdmin,verifyUser, EventCreatingController.getEvents);
+router.get('/getEvents', verifyAdminOrUser, EventCreatingController.getEvents);
 router.delete('/deleteEvents', verifyAdmin, EventCreatingController.deleteEvent);
 
 export default router;
