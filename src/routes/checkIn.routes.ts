b9dfd7ac486970e486611d0a09ee4dd520694cfb
TusharAppinventiv <tushar.vatsa@appinventiv.com>
2023-09-20 16:23:22 +0530

    commited

--- src/routes/checkIn.routes.ts
+++ src/routes/checkIn.routes.ts
@@ -1,14 +1,23 @@
-import { CheckinController } from "../controllers/checkin.controller";
-import { verifyAdmin } from "../middlewares/admin.auth";
-import { checkIn, checkout, acceptCheckout} from "../middlewares/booking.validator";
-import express from 'express'
-import { verifyUser } from "../middlewares/users.auth";
+import express, { Router } from 'express';
+import { CheckinController } from '../controllers/checkin.controller';
+import { verifyAdmin } from '../middlewares/admin.auth';
+import { checkIn, checkout, acceptCheckout } from '../middlewares/booking.validator';
+import { verifyUser } from '../middlewares/users.auth';
 
-const router = express.Router();
+class CheckinRouter {
+  public router: Router = express.Router();
 
-router.post('/create', verifyAdmin,checkIn, CheckinController.createCheckin);
-router.post('/checkout',verifyUser, checkout, CheckinController.checkOut);
-router.post('/acceptCheckout', verifyAdmin,acceptCheckout, CheckinController.acceptCheckOut);
-router.post('/declineCheckout', verifyAdmin,acceptCheckout, CheckinController.declineCheckOut)
+  constructor() {
+    this.initializeRoutes();
+  }
 
-export default router;
+  private initializeRoutes(): void {
+    this.router.post('/create', verifyAdmin, checkIn, CheckinController.createCheckin);
+    this.router.post('/checkout', verifyUser, checkout, CheckinController.checkOut);
+    this.router.post('/acceptCheckout', verifyAdmin, acceptCheckout, CheckinController.acceptCheckOut);
+    this.router.post('/declineCheckout', verifyAdmin, acceptCheckout, CheckinController.declineCheckOut);
+  }
+}
+
+const checkinRouter = new CheckinRouter();
+export default checkinRouter.router;
