76f8e570c8cd0630d0f0bfcf6dbb649b33dfe61b
TusharAppinventiv <tushar.vatsa@appinventiv.com>
2023-09-20 16:17:39 +0530

    commited

--- src/routes/rooms.routes.ts
+++ src/routes/rooms.routes.ts
@@ -1,20 +1,29 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { RoomCreatingController } from '../controllers/rooms.controller';
+import { BookingController } from '../controllers/booking.controller';
+import {roomValidator, bookingValidator, bookingConfirmation, BookingDeny} from '../middlewares/booking.validator'; 
 import { verifyAdmin } from '../middlewares/admin.auth';
-import BookingController from '../controllers/booking.controller';
-import {roomValidator} from '../middlewares/roomValidator'
 import { verifyUser } from '../middlewares/users.auth';
-import { bookingValidator, bookingConfirmation, BookingDeny } from '../middlewares/booking.validator';
 import { verifyAdminOrUser } from '../middlewares/verifyAdminOrUser';
-const router = express.Router();
 
-router.post('/create', verifyAdmin,roomValidator, RoomCreatingController.createRoom);
-router.get('/getRooms', verifyAdminOrUser, RoomCreatingController.getRooms);
-router.put('/set-available/:roomId', verifyAdmin, RoomCreatingController.setRoomAvailabilityToTrue);
-router.put('/set-unavailable/:roomId', verifyAdmin, RoomCreatingController.setRoomAvailabilityToFalse);
-router.post('/bookRooms', verifyUser, bookingValidator, BookingController.createBooking);
-router.post('/confirmBooking',  bookingConfirmation, BookingController.acceptBooking);
-router.post('/denyBooking', verifyAdmin, BookingDeny, BookingController.denyBooking );
-router.get('/available-rooms', BookingController.getAvailableRooms);
+class RoomBookingRouter {
+  public router: Router = express.Router();
 
-export default router
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router.post('/create', verifyAdmin, roomValidator, RoomCreatingController.createRoom);
+    this.router.get('/getRooms', verifyAdminOrUser, RoomCreatingController.getRooms);
+    this.router.put('/set-available/:roomId', verifyAdmin, RoomCreatingController.setRoomAvailabilityToTrue);
+    this.router.put('/set-unavailable/:roomId', verifyAdmin, RoomCreatingController.setRoomAvailabilityToFalse);
+    this.router.post('/bookRooms', verifyUser, bookingValidator, BookingController.createBooking);
+    this.router.post('/confirmBooking', verifyAdmin, bookingConfirmation, BookingController.acceptBooking);
+    this.router.post('/denyBooking', verifyAdmin, BookingDeny, BookingController.denyBooking);
+    this.router.get('/available-rooms', BookingController.getAvailableRooms);
+  }
+}
+
+const roomBookingRouter = new RoomBookingRouter();
+export default roomBookingRouter.router;
