d583cb6ed923285aa665724af07c24986db28656
TusharAppinventiv <tushar.vatsa@appinventiv.com>
2023-09-20 16:13:31 +0530

    commited

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,23 +1,29 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { UserController } from '../controllers/users.controller';
-import { verifyUser } from '../middlewares/users.auth';
 import { verifyAdmin } from '../middlewares/admin.auth';
-import { signupValidator,loginValidator, updateUserDataValidator, tokenAndPasswordValidationMiddleware } from '../middlewares/validator';
+import { signupValidator, loginValidator, updateUserDataValidator, tokenAndPasswordValidationMiddleware,} from '../middlewares/validator';
 import { verifyAdminOrUser } from '../middlewares/verifyAdminOrUser';
-const router = express.Router();
 
-router.post('/signup', signupValidator, UserController.signup);
-router.post('/login',loginValidator, UserController.login);
-router.post('/logout', UserController.logout);
-router.get('/protected-resource', verifyUser, (req, res) => {
-    res.status(200).json({ message: 'You have access to this protected resource.' });
-  });
-router.get('/confirm/:confirmationToken', UserController.confirmAccount);
-router.delete('/delete', verifyAdmin, UserController.deleteUser);
-router.get('/getUsers', verifyAdmin, UserController.getUsers);
-router.put('/:id/deactivate', verifyAdmin, UserController.deactivateUser);
-router.put('/update',verifyAdminOrUser,updateUserDataValidator, UserController.updateUser);
-router.post('/forgot-password', UserController.sendForgotPasswordEmail);
-router.post('/reset-password', tokenAndPasswordValidationMiddleware,UserController.resetPassword);
+class UserRouter {
+  public router: Router = express.Router();
 
-  export default router;
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router.post('/signup', signupValidator, UserController.signup);
+    this.router.post('/login', loginValidator, UserController.login);
+    this.router.post('/logout', verifyAdminOrUser, UserController.logout);
+    this.router.get('/confirm/:confirmationToken', UserController.confirmAccount);
+    this.router.delete('/delete', verifyAdmin, UserController.deleteUser);
+    this.router.get('/getUsers', verifyAdmin, UserController.getUsers);
+    this.router.put('/:id/deactivate', verifyAdmin, UserController.deactivateUser);
+    this.router.put('/update', verifyAdminOrUser, updateUserDataValidator, UserController.updateUser);
+    this.router.post('/forgot-password', UserController.sendForgotPasswordEmail);
+    this.router.post('/reset-password', tokenAndPasswordValidationMiddleware, UserController.resetPassword);
+  }
+}
+
+const userRouter = new UserRouter();
+export default userRouter.router;
